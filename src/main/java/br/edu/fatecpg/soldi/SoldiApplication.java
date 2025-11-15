7f0ce5b5aa29eb33ea517a8a4d1c79261b8a3b19
Thiago M. <185578934+thiimont@users.noreply.github.com>
2025-11-15 02:29:16 -0300

    Initial commit

---
+++ src/main/java/br/edu/fatecpg/soldi/SoldiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.soldi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoldiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoldiApplication.class, args);
+	}
+
+}
