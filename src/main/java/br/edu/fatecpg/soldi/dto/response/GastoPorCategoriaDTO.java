7e7013b87ddb2983a72f182a5a284ce15df3ca46
Frank Oliveira <fm0325836@gmail.com>
2025-11-16 15:16:27 -0300

    feat: add GastoPorCategoriaDTO and TransacaoResumoDTO with necessary fields

--- src/main/java/br/edu/fatecpg/soldi/dto/response/GastoPorCategoriaDTO.java
+++ src/main/java/br/edu/fatecpg/soldi/dto/response/GastoPorCategoriaDTO.java
@@ -1,4 +1,19 @@
 package br.edu.fatecpg.soldi.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GastoPorCategoriaDTO {
+    private String categoria;
+    private BigDecimal total;
+    private Long quantidadeTransacoes;
+    private Double percentual; // Percentual em relação ao total de despesas
 }
--- src/main/java/br/edu/fatecpg/soldi/dto/response/TransacaoResumoDTO.java
+++ src/main/java/br/edu/fatecpg/soldi/dto/response/TransacaoResumoDTO.java
@@ -1,4 +1,23 @@
 package br.edu.fatecpg.soldi.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransacaoResumoDTO {
+    private UUID uuid;
+    private String tipo;
+    private BigDecimal valor;
+    private String descricao;
+    private String categoria;
+    private LocalDateTime dataTransacao;
 }
