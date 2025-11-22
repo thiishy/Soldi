930bdffcf2d19592e11f56e98cc3f595804c1d93
Thiago M. <185578934+thiimont@users.noreply.github.com>
2025-11-21 23:57:53 -0300

    feat(config): add SecurityFilter to detect authorization in headers and call validation method

---
+++ backend/src/main/java/br/edu/fatecpg/soldi/config/SecurityFilter.java
@@ -0,0 +1,39 @@
+package br.edu.fatecpg.soldi.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+    private final TokenConfig tokenConfig;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizedHeader = request.getHeader("Authorization");
+        if (Strings.isNotEmpty(authorizedHeader) && authorizedHeader.startsWith("Bearer ")) {
+            String token = authorizedHeader.substring("Bearer ".length());
+            Optional<JWTUserData> optUser = tokenConfig.validateToken(token);
+
+            if(optUser.isPresent()) {
+                JWTUserData userData = optUser.get();
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userData, null, null);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
