04322f963b6c757ae73c2130f60547e66df57bfa
Thiago M. <185578934+thiimont@users.noreply.github.com>
2025-11-22 21:17:49 -0300

    feat(exception): create CustomAccessDeniedHandler handler for 403 forbidden requests

---
+++ backend/src/main/java/br/edu/fatecpg/soldi/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,36 @@
+package br.edu.fatecpg.soldi.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    private final ObjectMapper mapper;
+
+    public CustomAccessDeniedHandler() {
+        mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       org.springframework.security.access.AccessDeniedException accessDeniedException) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+
+        PermissionErrorMessage error = new PermissionErrorMessage(LocalDateTime.now(), HttpServletResponse.SC_FORBIDDEN, "Acesso negado");
+
+        response.getWriter().write(mapper.writeValueAsString(error));
+    }
+}
+
