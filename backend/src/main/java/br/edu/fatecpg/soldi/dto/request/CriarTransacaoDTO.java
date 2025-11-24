b6a292c69071dad825ecc547a8ab95831aaed92d
Thiago M. <185578934+thiimont@users.noreply.github.com>
2025-11-24 01:22:29 -0300

    fix(dto): change validations to notblank in CriarTransacaoDTO and RegistrarRequestDTO

--- backend/src/main/java/br/edu/fatecpg/soldi/dto/request/CriarTransacaoDTO.java
+++ backend/src/main/java/br/edu/fatecpg/soldi/dto/request/CriarTransacaoDTO.java
@@ -1,12 +1,12 @@
 package br.edu.fatecpg.soldi.dto.request;
 
 import br.edu.fatecpg.soldi.model.TipoTransacao;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
-public record CriarTransacaoDTO(@NotNull(message = "O tipo é obrigatório. (RECEITA ou DESPESA)") TipoTransacao tipo,
-                                @NotNull(message = "O valor é obrigatório.") @Positive(message = "O valor deve ser positivo.") BigDecimal valor,
+public record CriarTransacaoDTO(@NotBlank(message = "O tipo é obrigatório. (RECEITA ou DESPESA)") TipoTransacao tipo,
+                                @NotBlank(message = "O valor é obrigatório.") @Positive(message = "O valor deve ser positivo.") BigDecimal valor,
                                 String descricao,
-                                @NotNull(message = "A categoria é obrigatória.") String categoria) {}
+                                @NotBlank(message = "A categoria é obrigatória.") String categoria) {}
--- backend/src/main/java/br/edu/fatecpg/soldi/dto/request/RegistrarRequestDTO.java
+++ backend/src/main/java/br/edu/fatecpg/soldi/dto/request/RegistrarRequestDTO.java
@@ -1,9 +1,10 @@
 package br.edu.fatecpg.soldi.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
-public record RegistrarRequestDTO(@NotEmpty(message = "O nome é obrigatório.") String nome,
-                                  @Email @NotEmpty(message = "O email é obrigatório.") String email,
-                                  @NotEmpty(message = "A senha é obrigatória.") String senha,
-                                  @NotEmpty(message = "A confirmação da senha é obrigatória.") String confirmarSenha) { }
+public record RegistrarRequestDTO(@NotBlank(message = "O nome é obrigatório.") String nome,
+                                  @Email @NotBlank(message = "O email é obrigatório.") String email,
+                                  @NotBlank(message = "A senha é obrigatória.") String senha,
+                                  @NotBlank(message = "A confirmação da senha é obrigatória.") String confirmarSenha) { }
